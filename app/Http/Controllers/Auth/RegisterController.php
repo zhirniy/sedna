7817f9e753cd2ca5a562cb1b625c00ebfa86a99c
Denis Zhirniy <denis_777@ukr.net>
2019-10-18 11:36:11 +0300

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use App\User;
+
 
 class RegisterController extends Controller
 {
